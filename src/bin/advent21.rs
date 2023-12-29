20c94ea5b114cef8d580e26637f37a22b657a00c
DomNomNomVR <108088541+DomNomNomVR@users.noreply.github.com>
2023-12-30 06:33:35 +1300

    21

--- src/bin/advent21.rs
+++ src/bin/advent21.rs
@@ -83,6 +83,7 @@ fn main() {
     dbg!(answer1);
 
     // part2
+
     // We use the insight that after a certain amount of steps,
     // anything within a certain amount of steps from the start tends to repeat.
     // so then grow a region of space that we don't need to simulate anymore.
@@ -90,65 +91,132 @@ fn main() {
     s_row += ((26501365 / ht) + 2) * ht;
     s_col += ((26501365 / wd) + 2) * wd;
     let mut possibilities: HashSet<(usize, usize)> = HashSet::from([(s_row, s_col)]);
-    let mut count_histories: VecDeque<[usize; 4]> = VecDeque::new();
-    let mut unsimulated_count: [usize; 2] = [0, 0];
-    let mut simulated_radius = 0usize;
-    count_histories.push_back([0, 0, 0, usize::MAX]); // radius 0
-    count_histories.push_back([0, 0, 0, usize::MAX]); // radius 1
-    count_histories.push_back([0, 0, 0, usize::MAX]); // radius 2
-    count_histories.push_back([0, 0, 0, usize::MAX]); // radius 3
 
+    dbg!(wd);
+    dbg!(ht);
+
+    // turns out we are on a repeating pattern, so we can extrapolate with a polynomial
     let radius = |row: &usize, col: &usize| row.abs_diff(s_row) + col.abs_diff(s_col);
-    let target_iterations = 50usize;
+    // dbg!(26501365 % wd);
+    let polyfit = |i: usize| {
+        let x = i - 65;
+        assert_eq!(x % wd, 0);
+        let x = x / wd;
+        15427 * x * x + 15818 * x + 4052
+    };
+
+    let target_iterations = 999usize;
     for i in 0..target_iterations {
-        // update next_count_histories
-        let i_mod4 = i % 4;
-        for hist4 in count_histories.iter_mut() {
-            hist4[i_mod4] = 0;
+        if i % wd == 65 {
+            println!("{},{}", i, polyfit(i));
         }
-        for (row, col) in possibilities.iter() {
-            let hist_index = radius(row, col) - simulated_radius;
-            if hist_index >= count_histories.len() {
-                continue;
-            }
-            count_histories[hist_index][i_mod4] += 1;
-        }
-
-        // increase the unsimulated_radius if possible
-        while count_histories[0][1] == count_histories[0][3]  // micro optimization: do the check that's more likely to fail first.
-            && count_histories[0][0] == count_histories[0][2]
-        {
-            unsimulated_count[0] += count_histories[0][0];
-            unsimulated_count[1] += count_histories[0][1];
-            count_histories.pop_front();
-            count_histories.push_back([0, 0, 0, usize::MAX]);
-            simulated_radius += 1;
-            if simulated_radius % 100 == 0 {
-                println!("grew at i={i} to r={simulated_radius}");
-            }
-        }
-
-        // grow the possibilities
-        possibilities = possibilities
-            .iter()
-            .flat_map(|&(row, col)| {
-                [
-                    (row - 1, col),
-                    (row, col - 1),
-                    (row + 1, col),
-                    (row, col + 1),
-                ]
-            })
-            .filter(|(row, col)| {
-                radius(row, col) >= simulated_radius && !rocks.contains(&(row % ht, col % wd))
-            })
-            .collect();
     }
+    // 631357648815452 is too high
+    dbg!(polyfit(26501365));
 
-    dbg!(unsimulated_count[target_iterations % 2]);
-    dbg!(possibilities.len());
-    let answer2 = unsimulated_count[target_iterations % 2] + possibilities.len();
-    dbg!(answer2);
+    // 65,4052
+    // 196,35297
+    // 327,97396
+    // 458,190349
+    // 589,314156
+    // 720,468817
+    // 851,654332
+    // 982,870701
+
+    // for i in 0..target_iterations {
+    //     // grow the possibilities
+    //     possibilities = possibilities
+    //         .iter()
+    //         .flat_map(|&(row, col)| {
+    //             [
+    //                 (row - 1, col),
+    //                 (row, col - 1),
+    //                 (row + 1, col),
+    //                 (row, col + 1),
+    //             ]
+    //         })
+    //         .filter(|(row, col)| !rocks.contains(&(row % ht, col % wd)))
+    //         .collect();
+    //     if i % wd == 65 {
+    //         println!("{},{}", i, possibilities.len());
+    //     }
+    // }
+
+    // 0,3788
+    // 1,34509
+    // 2,96084
+    // 3,188513
+
+    // let answer2 = possibilities.len();
+    // dbg!(answer2);
+
+    // // We use the insight that after a certain amount of steps,
+    // // anything within a certain amount of steps from the start tends to repeat.
+    // // so then grow a region of space that we don't need to simulate anymore.
+    // let &(mut s_row, mut s_col) = start.iter().next().unwrap();
+    // s_row += ((26501365 / ht) + 2) * ht;
+    // s_col += ((26501365 / wd) + 2) * wd;
+    // let mut possibilities: HashSet<(usize, usize)> = HashSet::from([(s_row, s_col)]);
+    // let mut count_histories: VecDeque<[usize; 4]> = VecDeque::new();
+    // let mut unsimulated_count: [usize; 2] = [0, 0];
+    // let mut simulated_radius = 0usize;
+    // count_histories.push_back([0, 0, 0, usize::MAX]); // radius 0
+    // count_histories.push_back([0, 0, 0, usize::MAX]); // radius 1
+    // count_histories.push_back([0, 0, 0, usize::MAX]); // radius 2
+    // count_histories.push_back([0, 0, 0, usize::MAX]); // radius 3
+
+    // let radius = |row: &usize, col: &usize| row.abs_diff(s_row) + col.abs_diff(s_col);
+    // let target_iterations = 50usize;
+    // for i in 0..target_iterations {
+    //     // update next_count_histories
+    //     let i_mod4 = i % 4;
+    //     for hist4 in count_histories.iter_mut() {
+    //         hist4[i_mod4] = 0;
+    //     }
+    //     for (row, col) in possibilities.iter() {
+    //         let hist_index = radius(row, col) - simulated_radius;
+    //         if hist_index >= count_histories.len() {
+    //             continue;
+    //         }
+    //         count_histories[hist_index][i_mod4] += 1;
+    //     }
+
+    //     // increase the unsimulated_radius if possible
+    //     while count_histories[0][1] == count_histories[0][3]  // micro optimization: do the check that's more likely to fail first.
+    //         && count_histories[0][0] == count_histories[0][2]
+    //     {
+    //         unsimulated_count[0] += count_histories[0][0];
+    //         unsimulated_count[1] += count_histories[0][1];
+    //         count_histories.pop_front();
+    //         count_histories.push_back([0, 0, 0, usize::MAX]);
+    //         simulated_radius += 1;
+    //         if simulated_radius % 100 == 0 {
+    //             println!("grew at i={i} to r={simulated_radius}");
+    //         }
+    //     }
+
+    //     // grow the possibilities
+    //     possibilities = possibilities
+    //         .iter()
+    //         .flat_map(|&(row, col)| {
+    //             [
+    //                 (row - 1, col),
+    //                 (row, col - 1),
+    //                 (row + 1, col),
+    //                 (row, col + 1),
+    //             ]
+    //         })
+    //         .filter(|(row, col)| {
+    //             radius(row, col) >= simulated_radius && !rocks.contains(&(row % ht, col % wd))
+    //         })
+    //         .collect();
+    // }
+
+    // dbg!(unsimulated_count[target_iterations % 2]);
+    // dbg!(possibilities.len());
+    // let answer2 = unsimulated_count[target_iterations % 2] + possibilities.len();
+    // dbg!(answer2);
+
     // let foo = possibilities
     //     .iter()
     //     .map(|(row, col)| radius(row, col))
