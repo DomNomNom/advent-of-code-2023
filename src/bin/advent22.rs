8b0bee51570cf20c6ddd16e5747f945862ce4808
DomNomNomVR <108088541+DomNomNomVR@users.noreply.github.com>
2023-12-30 17:28:27 +1300

    22

--- src/bin/advent22.rs
+++ src/bin/advent22.rs
@@ -44,32 +44,68 @@ fn main() {
         })
         .collect_vec();
 
+    let to_letter: HashMap<aabb, char> = HashMap::from_iter(
+        input
+            .clone()
+            .into_iter()
+            .enumerate()
+            .map(|(i, b)| (b, "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().nth(i).unwrap())),
+    );
+
+    for (j, down) in input.iter().enumerate() {
+        for (i, up) in input.iter().enumerate() {
+            if up.0[2] < down.0[2] || i == j {
+                continue; // wrong direction
+            }
+            if intersects_xy(up, down) {
+                println!("{} -upish-> {}", to_letter[down], to_letter[up]);
+            }
+        }
+    }
+    println!();
+
     input.sort_by_key(|brick| brick.1[2]); // lowest top-edge first
+    let to_letter: HashMap<usize, char> =
+        HashMap::from_iter(input.iter().enumerate().map(|(i, b)| (i, to_letter[b])));
 
     // make the blocks fall and build dependency graph
     let mut to_down: HashMap<usize, Vec<usize>> = HashMap::new();
     let mut to_up: HashMap<usize, Vec<usize>> = HashMap::new();
     let mut fallen: Vec<aabb> = vec![];
-    for (i, mut falling) in input.into_iter().enumerate() {
+    for (i, mut up) in input.into_iter().enumerate() {
         let mut support_z: Option<u16> = None;
-        for (j, support) in fallen.iter().enumerate().rev() {
+        println!("{:?}", fallen.iter().enumerate().rev().collect_vec());
+        for (j, down) in fallen.iter().enumerate().rev() {
             if let Some(z) = support_z {
-                if support.1[2] < z {
+                if down.1[2] > z {
+                    unreachable!("impossibru");
+                }
+                if down.1[2] < z {
                     break;
                 }
             }
-            if intersects_xy(&falling, support) {
-                support_z = Some(support.1[2]);
+            if intersects_xy(&up, down) {
+                support_z = Some(down.1[2]);
                 (*to_down.entry(i).or_default()).push(j);
+
+                println!(
+                    "{} -upaa-> {} at z={}",
+                    to_letter[&j],
+                    to_letter[&i],
+                    support_z.unwrap()
+                );
             }
         }
         if let Some(z) = support_z {
-            move_to_z(&mut falling, z);
+            move_to_z(&mut up, z);
+            println!("on block: {} z={}..{}", to_letter[&i], up.0[2], up.1[2]);
         } else {
-            move_to_z(&mut falling, 1);
-            fallen.push(falling);
+            move_to_z(&mut up, 1);
             assert_eq!(to_down.insert(i, vec![]), None);
+            println!("on ground: {}  z={}..{}", to_letter[&i], up.0[2], up.1[2]);
         }
+        fallen.push(up);
+        fallen.sort_by_key(|brick| brick.1[2]); // lowest top-edge first
     }
 
     for (up, downs) in to_down.iter() {
@@ -82,9 +118,24 @@ fn main() {
             to_up.insert(i, vec![]);
         }
     }
+
+    for (i, ups) in to_up.iter().sorted_by_key(|(i, _)| to_letter[i]) {
+        println!(
+            "{} -up-> {:?}",
+            to_letter[i],
+            ups.iter().map(|up| to_letter[up]).collect_vec()
+        )
+    }
+    for (i, downs) in to_down.iter().sorted_by_key(|(i, _)| to_letter[i]) {
+        println!(
+            "{} -down-> {:?}",
+            to_letter[i],
+            downs.iter().map(|down| to_letter[down]).collect_vec()
+        )
+    }
     let answer1 = to_up
         .iter()
-        .filter(|&(k, ups)| ups.iter().all(|up| to_down[up].len() > 1))
+        .filter(|&(_k, ups)| ups.iter().all(|up| to_down[up].len() > 1))
         .count();
     dbg!(answer1);
     // for brick in bricks {}
