1012dbbe07fa96a95189a4798eb693300dfbc8c2
DomNomNomVR <108088541+DomNomNomVR@users.noreply.github.com>
2024-01-01 01:22:56 +1300

    24

--- src/bin/advent24.rs
+++ src/bin/advent24.rs
@@ -1,6 +1,8 @@
 use itertools::Itertools;
-use std::{collections::HashSet, fs::read};
-
+use ordered_float::OrderedFloat;
+use rand::prelude::*;
+use rand_distr::Normal;
+use std::fs::read;
 use vec3D::Vec3D;
 
 type PosVel = (Vec3D, Vec3D);
@@ -31,30 +33,26 @@ fn approach_times(track1: &PosVel, track2: &PosVel) -> (f64, f64) {
     // return norm(dP);   // return the closest distance
 }
 
-fn main() {
-    let input = read("inputs/24.txt").unwrap();
-    let input = String::from_utf8(input).unwrap();
-    let input = input.lines().collect_vec();
-    let mut particles = input
-        .into_iter()
-        .map(|line| {
-            let (pos, vel) = line.split_once(" @ ").unwrap();
-            let pos = pos
-                .split(", ")
-                .map(|x| x.parse::<f64>().unwrap())
-                .collect_vec();
-            let vel = vel
-                .split(", ")
-                .map(|x| x.trim().parse::<f64>().unwrap())
-                .collect_vec();
-            (
-                Vec3D::new(pos[0], pos[1], pos[2]),
-                Vec3D::new(vel[0], vel[1], vel[2]),
-            )
-        })
-        .collect_vec();
+// computes the time when the given two trajectories are closest together.
+fn cpa_time(a: &PosVel, b: &PosVel) -> f64 {
+    let dv = a.1 - b.1;
+    let dv2 = dv.dot(dv);
+    if dv2 < 0.0000001 {
+        return 0.0; // almost parallel. any time is ok.  Use time 0.
+    }
+    let w0 = a.0 - b.0;
+    -w0.dot(dv) / dv2
+}
 
-    // surely this won't cause problems later
+// computes the distance at the closest approach time
+fn cpa_distance(a: &PosVel, b: &PosVel) -> f64 {
+    let ctime = cpa_time(a, b);
+    let p1 = a.0 + (a.1 * ctime);
+    let p2 = b.0 + (b.1 * ctime);
+    p1.distance_to(p2) // distance at CPA
+}
+
+fn part1(mut particles: Vec<PosVel>) {
     for (ref mut pos, ref mut vel) in particles.iter_mut() {
         pos.z = 0.;
         vel.z = 0.;
@@ -112,4 +110,106 @@ fn main() {
         }
     }
     dbg!(answer1);
+}
+
+fn part2(particles: Vec<PosVel>) {
+    let answer2 = 0;
+    let cost_fn = |throw: &PosVel| -> f64 {
+        particles
+            .iter()
+            .map(|p| cpa_distance(throw, p))
+            .sum::<f64>()
+    };
+    let cost_fn2 = |sample: &[f64; 6]| -> f64 {
+        cost_fn(&(
+            // Vec3D::new(sample[0].floor(), sample[1].floor(), sample[2].floor()),
+            // Vec3D::new(sample[3].floor(), sample[4].floor(), sample[5].floor()),
+            Vec3D::new(sample[0], sample[1], sample[2]),
+            Vec3D::new(sample[3], sample[4], sample[5]),
+        ))
+    };
+
+    // dbg!(cost_fn2(&[24., 13., 10., -3., 1., 2.]));
+    // return;
+
+    // let mut samples =
+
+    let mut rng = StdRng::seed_from_u64(4);
+    let mut distribution: [Normal<f64>; 6] = [
+        // Normal::new(319458320561568., 2676204445423380000.).unwrap(),
+        // Normal::new(319458320561568., 2676204445423380000.).unwrap(),
+        // Normal::new(319458320561568., 2676204445423380000.).unwrap(),
+        // Normal::new(0., 10000.).unwrap(),
+        // Normal::new(0., 10000.).unwrap(),
+        // Normal::new(0., 10000.).unwrap(),
+        Normal::new(20., 10.).unwrap(),
+        Normal::new(10., 10.).unwrap(),
+        Normal::new(10., 10.).unwrap(),
+        Normal::new(0., 10.).unwrap(),
+        Normal::new(0., 10.).unwrap(),
+        Normal::new(0., 10.).unwrap(),
+    ];
+
+    for _ in 0..1000 {
+        let sample_size = 20000;
+        let elite_size = 100;
+        let mut samples = (0..sample_size)
+            .map(|_| distribution.map(|dist| dist.sample(&mut rng)))
+            .collect_vec();
+        samples.sort_by_cached_key(|sample| OrderedFloat(cost_fn2(sample)));
+        // dbg!(samples.iter().map(cost_fn2).collect_vec());
+
+        // Fit a new distribution to the best samples
+        let elites = samples[0..elite_size].to_vec();
+        dbg!(cost_fn2(&elites[elite_size - 1]));
+        let fit = |i| {
+            let mean = elites.iter().map(|sample| sample[i]).sum::<f64>() / elite_size as f64;
+            let deviation = (elites
+                .iter()
+                .map(|sample| {
+                    let diff = sample[i] - mean;
+                    diff * diff
+                })
+                .sum::<f64>()
+                / elite_size as f64)
+                .sqrt();
+            let deviation = deviation * 1.5;
+            Normal::new(mean, deviation).unwrap()
+        };
+        distribution = [fit(0), fit(1), fit(2), fit(3), fit(4), fit(5)];
+        // dbg!(distribution.map(|d| d.std_dev()));
+        if distribution.iter().all(|d| d.std_dev() < 0.1) {
+            println!("converged!");
+            break;
+        }
+    }
+    dbg!(distribution.map(|normal| normal.mean()));
+    dbg!(answer2);
+}
+
+fn main() {
+    let input = read("inputs/24test.txt").unwrap();
+    let input = String::from_utf8(input).unwrap();
+    let input = input.lines().collect_vec();
+    let mut particles = input
+        .into_iter()
+        .map(|line| {
+            let (pos, vel) = line.split_once(" @ ").unwrap();
+            let pos = pos
+                .split(", ")
+                .map(|x| x.parse::<f64>().unwrap())
+                .collect_vec();
+            let vel = vel
+                .split(", ")
+                .map(|x| x.trim().parse::<f64>().unwrap())
+                .collect_vec();
+            (
+                Vec3D::new(pos[0], pos[1], pos[2]),
+                Vec3D::new(vel[0], vel[1], vel[2]),
+            )
+        })
+        .collect_vec();
+
+    part1(particles.clone());
+    part2(particles);
 }
